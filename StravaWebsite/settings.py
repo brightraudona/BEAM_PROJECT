7253760b250d3f9f4f8f55f88533cded63f1b398
Matas Togobickij <matas.togobickij@ktu.edu>
2023-03-23 16:34:12 +0200

    remove dotenv library

--- StravaWebsite/settings.py
+++ StravaWebsite/settings.py
@@ -13,11 +13,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 from django.core.management.utils import get_random_secret_key
 
-from dotenv import load_dotenv
-load_dotenv()
-import os
-
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
