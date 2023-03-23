865280fa679684dca8e504f94b6b1a9b265d9c65
Artur Maslov <arturthemaslov@gmail.com>
2023-03-23 19:46:56 +0300

    improve views and html logic + add map

--- App/views.py
+++ App/views.py
@@ -1,20 +1,19 @@
 from django.shortcuts import render
 from django.contrib.auth import logout
+from folium import folium
 import requests
 
 # Create your views here.
 def home(request):
     # Make your map object
-    # main_map = folium.Map(location=[43.45, -80.476], zoom_start = 12) # Create base map
-    # main_map_html = main_map._repr_html_() # Get HTML for website
+    main_map = folium.Map(location=[54.6872, 25.2797], zoom_start = 12) # Create base map
+    main_map_html = main_map._repr_html_() # Get HTML for website
 
     if request.user.is_anonymous:
         return render(request, 'index.html')
-
-    user = request.user # Pulls in the Strava User data
-    strava_login = user.social_auth.get(provider='strava') # Strava login
-    #email = strava_login.extra_data['email']
-    data = {
-        "user":request.user
-    }
-    return render(request, 'index.html', data)
+    else:
+        data = {
+            "user":request.user,
+            "main_map":main_map_html
+        }
+        return render(request, 'index.html', data)
