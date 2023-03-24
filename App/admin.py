05778e9470168be194e41ba9ef7e7591de18e830
benjaminas <benjaminas.togobickij@vdu.lt>
2023-03-24 06:17:22 +0200

    admin without activities

--- App/admin.py
+++ App/admin.py
@@ -1,5 +1,13 @@
+from django import forms
 from django.contrib import admin
-from .models import Activity, Challenge
+from .models import Challenge
 
-admin.site.register(Activity)
-admin.site.register(Challenge)
+class ChallengeForm(forms.ModelForm):
+    class Meta:
+        model = Challenge
+        exclude = ('activities',)
+
+class ChallengeAdmin(admin.ModelAdmin):
+    form = ChallengeForm
+
+admin.site.register(Challenge, ChallengeAdmin)
